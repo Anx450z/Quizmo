ce3c881ddf9706f96660c746f1cadca0f014accb
Ankur <anx450z@gmail.com>
2023-04-24 23:34:59 +0530

    fixed prettier

--- app/javascript/src/index.tsx
+++ app/javascript/src/index.tsx
@@ -2,16 +2,13 @@ import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-document.addEventListener("turbo:load", () => {
-  const root = ReactDOM.createRoot(
-    document.body.appendChild(document.createElement("div"))
-  )
+document.addEventListener('turbo:load', () => {
+  const root = ReactDOM.createRoot(document.body.appendChild(document.createElement('div')))
   root.render(
     <React.StrictMode>
-    <Suspense fallback={<div>Loading...</div>}>
-      <App />
-    </Suspense>
-  </React.StrictMode>
-)
+      <Suspense fallback={<div>Loading...</div>}>
+        <App />
+      </Suspense>
+    </React.StrictMode>
+  )
 })
-
