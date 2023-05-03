612a5e09ce26194cd505bd3816c680cda6cd0850
Ankur <anx450z@gmail.com>
2023-05-03 22:24:08 +0530

    added user model routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
+  get 'user/create'
+  get 'user/update'
+  get 'user/destroy'
+  get 'user/show'
+  root to: 'home#index'
+  get '*path', to: 'home#index'
+
   resources :quiz do
     resources :question, only: %i[create update destroy]
   end
 
   post '/question/:question_id/option', to: 'option#create'
-  patch '/question/:question_id/option/:id', to: 'option#update'
-  put '/question/:question_id/option/:id', to: 'option#update'
+  patch '/option/:id', to: 'option#update'
+  put '/option/:id', to: 'option#update'
   delete '/option/:id', to: 'option#destroy'
 
-  root to: 'home#index'
-  get '*path', to: 'home#index'
+  resources :user ,only: %i[create update destroy show]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_30_171555) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_03_163710) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_30_171555) do
   create_table "quizzes", force: :cascade do |t|
     t.string "title", null: false
     t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
