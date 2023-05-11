3cdc751abbffc16e549d741b4822ab697b5cf6f4
Ankur <anx450z@gmail.com>
2023-05-11 23:00:49 +0530

    fixed react quill

--- app/javascript/src/components/question/CreateQuestion.tsx
+++ app/javascript/src/components/question/CreateQuestion.tsx
@@ -2,26 +2,27 @@ import React from 'react'
 import QuestionForm from './QuestionForm'
 import { useParams } from 'react-router-dom'
 import questionApi from '../../apis/questions'
+import ReactQuill from 'react-quill'
 
-const CreateQuestion = (props:any) => {
-
-  const {id} = useParams()
-
-  const handleOnSubmit = async (e:any) => {
+const CreateQuestion = (props: any) => {
+  const { id } = useParams()
+  
+  
+  const handleOnSubmit = async (e: any) => {
     const data = new FormData(e.currentTarget)
     const question = {
-      question: data.get('question') as string
+      question: data.get('question') as string,
     }
     await questionApi.createQuestion(id!, question)
     props.onMutate()
   }
-  
+
   return (
     <>
-      <label className='pill bg-red-100 text-red-500'>{props.newIndex}</label>
+      <label className="pill bg-red-100 text-red-500">{props.newIndex}</label>
       <label>Add new question</label>
-      <br></br>
-      <QuestionForm button="Add" onSubmit={handleOnSubmit}/>
+      
+      <QuestionForm button="Add" onSubmit={handleOnSubmit} />
     </>
   )
 }
--- app/javascript/src/components/question/QuestionForm.tsx
+++ app/javascript/src/components/question/QuestionForm.tsx
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ReactQuill from 'react-quill'
 
-const QuestionForm = (props:any) => {
+const QuestionForm = (props: any) => {
+  const [value, setValue] = useState('')
+  const modules = {
+    toolbar: [
+      [{ font: [] }],
+      ['bold', 'italic', 'underline'],
+      [{ script: 'sub' }, { script: 'super' }],
+      ['blockquote', 'code-block'],
+      [{ list: 'ordered' }, { list: 'bullet' }],
+    ],
+  }
   return (
-    <div className='flex-col'> 
-      <form id='question' onSubmit={props.onSubmit}>
-          {/* <label>Enter your Question : </label> */}
-          <br></br>
-          <textarea placeholder='Your question goes here' id="question" name="question"></textarea>
-          <br></br>
-          <button type='submit'>{props.button}</button>
+    <div className="flex-col">
+      <form id="question" onSubmit={props.onSubmit}>
+        {/* <label>Enter your Question : </label> */}
+        <br></br>
+        <textarea placeholder="Your question goes here" id="question" name="question"></textarea>
+        <div className="max-w-2xl rounded-2xl border border-b-4 py-2">
+          <ReactQuill
+            placeholder="Add new question here..."
+            modules={modules}
+            value={value}
+            onChange={setValue}
+            theme="snow"
+            className="bg-white text-3xl"
+          />
+        </div>
+        <br></br>
+        <button type="submit">{props.button}</button>
       </form>
     </div>
   )
