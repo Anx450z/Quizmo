00a118866c73ce7f724abfd2f60700257e171890
Ankur <anx450z@gmail.com>
2023-06-19 00:49:14 +0530

    updated testcases

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
+  # *Quiz Settings routes
   get '/quiz/:quiz_id/setting', to: 'quiz_setting#show'
   post '/quiz/:quiz_id/setting', to: 'quiz_setting#create'
   put '/quiz/:quiz_id/setting', to: 'quiz_setting#update'
 
+  # *Quiz routes
   resources :quiz do
     resources :question, only: %i[create update destroy]
   end
+  get '/quiz/:id/preview',  to: 'quiz#preview'
+  get 'quiz/:id/score', to: 'quiz#score'
 
+  # *Options Routes
   post '/question/:question_id/option', to: 'option#create'
   patch '/option/:id', to: 'option#update'
   put '/option/:id', to: 'option#update'
@@ -14,20 +19,21 @@ Rails.application.routes.draw do
 
   resources :user, only: %i[create update destroy show]
 
+  # *Session routes
   post '/login', to: 'sessions#create'
   get '/is_logged_in', to: 'sessions#is_logged_in?'
   delete '/logout', to: 'sessions#destroy'
 
+  # *Marked_options routes
   post '/marked_option', to: 'marked_option#create'
   delete '/marked_option/delete/:question_id', to: 'marked_option#destroy'
-  get '/quiz/:id/preview',  to: 'quiz#preview'
-  get 'quiz/:id/score', to: 'quiz#score'
 
+  # *Token routes
   post '/quiz/:quiz_id/generate_tokens', to: 'token#create'
   get '/quiz/:quiz_id/show_tokens', to: 'token#show'
   get '/token/:token_code', to: 'token#show_quiz'
 
-  # below routes have to place at the bottom
+  # *below routes have to place at the bottom
   root to: 'home#index'
   get '*path', to: 'home#index'
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,11 +14,12 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'creating a new user' do
     context 'When all fields are set correctly' do
-      @user = User.create(username: 'John', email: 'john@example.com', password: 'password', password_confirmation: 'password')
-    end
-
-    it 'should be created successfully' do
-      expect(@user.username).to be('John')
+      before do
+        @user = User.create(username: 'John', email: 'john@example.com', password: 'password', password_confirmation: 'password')
+      end
+      it 'should be created successfully' do
+        expect(@user.username).to match('John')
+      end
     end
   end
 end
